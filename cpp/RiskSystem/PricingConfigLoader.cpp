c6fbbd5fa99741d501c93c8b9d68f67537c2b2a5
johanyim <johanjyyim@gmail.com>
2026-02-25 17:16:01 +0000

    xml parsing

--- cpp/RiskSystem/PricingConfigLoader.cpp
+++ cpp/RiskSystem/PricingConfigLoader.cpp
@@ -1,6 +1,24 @@
 #include "PricingConfigLoader.h"
+#include "RiskSystem/PricingEngineConfig.h"
+#include "third-party/pugixml.hpp"
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <stdexcept>
+
+PricingEngineConfig parseXml(const std::string& content)
+{
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_string(content.c_str());
+    if (!result) {
+        std::cerr << "Invalid XML: " << result.description() << '\n';
+    }
+
+    std::cout << "Managed to Parse" << "\n";
+
+    return PricingEngineConfig();
+}
 
 std::string PricingConfigLoader::getConfigFile() const { return configFile_; }
 
@@ -11,5 +29,15 @@ void PricingConfigLoader::setConfigFile(const std::string& file)
 
 PricingEngineConfig PricingConfigLoader::loadConfig()
 {
-    throw std::runtime_error("Not implemented");
+
+    std::ifstream file(configFile_);
+    if (!file) {
+        throw std::runtime_error("Failed to open file");
+    }
+
+    std::string file_str = std::string((std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>());
+    PricingEngineConfig config = parseXml(file_str);
+
+    return config;
 }
